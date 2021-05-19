fe249145ff1a3f30b15d0aadbd8e722822637682
Gert Vanbeckevoort <gert.vanbeckevoort@gmail.com>
2021-05-19 11:37:41 +0200

    reorganisation

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'footnotes.js',
+        library: {
+            name: "Footnotes",
+            type: 'umd',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/i,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "less-loader",
+                        options: {
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+}
