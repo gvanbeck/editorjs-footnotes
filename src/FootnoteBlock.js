600b88c17f02ae9a62f7a03f3df2938203c8b241
Gert Vanbeckevoort <gert.vanbeckevoort@gmail.com>
2021-05-28 13:06:42 +0200

    css and less implementation

--- src/FootnoteBlock.js
+++ src/FootnoteBlock.js
@@ -1,4 +1,6 @@
 import icon from './assets/sticky-notes.svg';
+import selectIcon from './assets/touchscreen.svg';
+import toggleSizeIcon from './assets/minimize.svg';
 import template from './templates/footnote-block.mustache';
 import jsu from 'handle-events';
 import { FootnoteTool } from './FootnoteTool.js';
@@ -18,15 +20,47 @@ export class FootnoteBlock {
         this.data = data;
     }
 
+    renderSettings(){
+        const settings = [
+            {
+                name: 'select',
+                icon: selectIcon
+            },
+            {
+                name: 'open/close',
+                icon: toggleSizeIcon
+            },
+        ];
+        const wrapper = document.createElement('div');
+
+        settings.forEach( tune => {
+            let button = document.createElement('div');
+
+            button.classList.add('cdx-settings-button');
+            button.innerHTML = tune.icon;
+            wrapper.appendChild(button);
+            jsu.addEventListener(button, 'click', () => {
+                this._toggleTune(tune.name);
+                button.classList.add('cdx-footnote-icon');
+                button.classList.toggle('cdx-settings-button--active');
+            });
+        });
+
+        return wrapper;
+    }
+
+    _toggleTune(tune) {
+        console.log('Image tune clicked', tune);
+    }
+
     render() {
         const container = document.createElement('div');
         container.classList.add('cdx-footnote-block');
         container.innerHTML = template({noteId:'#12345'});
         let elms = container.querySelectorAll('.fnb-content');
-        [].forEach.call(elms, (e) => {
+        forEach(elms, (e) => {
             e.contentEditable = true;
         });
-
 
         jsu.addEventListener(container, 'click.footnote', (e) => {
             const b = this.api.blocks;
--- src/FootnoteBlockTune.js
+++ src/FootnoteBlockTune.js
@@ -19,4 +19,9 @@ export class FootnoteBlockTune {
 
         return button; 
     }
+
+    wrap(blockContent) {
+        console.log(blockContent);
+        return blockContent;
+    }
 }
